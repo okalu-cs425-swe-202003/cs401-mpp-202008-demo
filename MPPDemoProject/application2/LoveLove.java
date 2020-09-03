568d54c245c926f97eba7644e51fd798dbe537b2
namhm <namhm@10.10.14.140>
2020-09-03 12:28:50 -0500

    Updated

--- MPPDemoProject/application2/LoveLove.java
+++ MPPDemoProject/application2/LoveLove.java
@@ -4,7 +4,7 @@ public class LoveLove {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("LoveLove");
+		System.out.println("Test test");
 	}
 
 }
