44c47b9f6bbc8ef13563dffcfed475ebeb0a8d2c
namhm <namhm@10.10.14.140>
2020-09-03 12:15:40 -0500

    updated by Nam

--- MPPDemoProject/application/Main.java
+++ MPPDemoProject/application/Main.java
@@ -15,7 +15,9 @@ public class Main extends Application {
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			System.out.println("Changes by somebody from #113..");
+			System.out.println("Changes by somebody from #113..");			
+			System.out.println("Changes by somebody from #115..");
+
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
