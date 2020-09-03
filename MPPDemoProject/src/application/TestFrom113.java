96626eeb3475dacdaa9972d6136f409f7a22f784
ganzorigu <ganzorigu@localhost>
2020-09-03 11:52:40 -0500

    added new test java class

---
+++ MPPDemoProject/src/application/TestFrom113.java
@@ -0,0 +1,10 @@
+package application;
+
+public class TestFrom113 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("just testing...");
+	}
+
+}
