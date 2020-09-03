d1dbd9d09243d55c722ca192d7a2ae6626ff90b9
ganzorigu <ganzorigu@localhost>
2020-09-03 11:48:41 -0500

    Somebody from #113

--- MPPDemoProject/src/application/Main.java
+++ MPPDemoProject/src/application/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			System.out.println("Changes by MOHAN");
+			System.out.println("Changes by somebody from #113.");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
